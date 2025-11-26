2ca00f222186391339078f1aa25521d2bc89a0ce
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-26 19:19:03 +0300

    Update dbconfig.php

--- cms/engine/data/dbconfig.php
+++ cms/engine/data/dbconfig.php
@@ -2,18 +2,10 @@
 
 define ("DBHOST", "localhost"); 
 
-define ("DBNAME", "");
+define ("DBNAME", "cajeerengine");
 
-define ("DBUSER", "");
+define ("DBUSER", "root");
 
-define ("DBPASS", "");  
+define ("DBPASS", "password");  
 
-define ("PREFIX", ""); 
-
-define ("USERPREFIX", "");
-
-define ("COLLATE", "utf-8"); 
-
-define('SECURE_AUTH_KEY', '#1 &c.G3[a<xd=>`GS5B%r~F?1&x>`1<|a>(MSe<!:#Gges73sN;s2ogE>ZF|/k>');
- 
 ?>
