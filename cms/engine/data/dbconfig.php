381ee0630785bf90b62b1f6ed91280a3f0ee0859
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-26 17:03:35 +0300

    Create dbconfig.php

---
+++ cms/engine/data/dbconfig.php
@@ -0,0 +1,19 @@
+<?PHP
+
+define ("DBHOST", "localhost"); 
+
+define ("DBNAME", "");
+
+define ("DBUSER", "");
+
+define ("DBPASS", "");  
+
+define ("PREFIX", ""); 
+
+define ("USERPREFIX", "");
+
+define ("COLLATE", "utf-8"); 
+
+define('SECURE_AUTH_KEY', '#1 &c.G3[a<xd=>`GS5B%r~F?1&x>`1<|a>(MSe<!:#Gges73sN;s2ogE>ZF|/k>');
+ 
+?>
