c6002e325a32cdecb93acbd56d331a79dc5f0568
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-27 13:38:45 +0300

    Update errors.php

--- cms/templates/errors.php
+++ cms/templates/errors.php
@@ -1,1 +1,10 @@
-
+<?php
+if ($_SESSION['USER_ERROR'] == 404) {
+	$_SESSION['USER_ERROR'] = '';
+	echo '<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>404 Not Found.</title></head><body></body></html>';
+}
+else if ($_SESSION['USER_ERROR'] == 403) {
+	$_SESSION['USER_ERROR'] = '';
+	echo '<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>403 Forbidden.</title></head><body></body></html>';
+}
+?>
