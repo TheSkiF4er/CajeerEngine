063f6b8540c0797085dc48c9c9f70059ad2c254d
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-12 13:54:45 +0300

    Add files via upload

--- engine/modules/news/Controller.php
+++ engine/modules/news/Controller.php
@@ -1,1 +1,21 @@
+<?php
+namespace Modules\news;
 
+use Template\Template;
+
+class Controller
+{
+    public function index(): void
+    {
+        $tpl = new Template();
+        $tpl->render('main.tpl', [
+            'title' => 'CajeerEngine',
+            'content' => 'News index (stub)',
+        ]);
+    }
+
+    public function view(): void
+    {
+        echo 'News view (stub)';
+    }
+}
