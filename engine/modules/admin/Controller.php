8f43bfede572234b2d225caee0bb89c550179553
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-12 13:53:56 +0300

    Add files via upload

--- engine/modules/admin/Controller.php
+++ engine/modules/admin/Controller.php
@@ -1,1 +1,16 @@
+<?php
+namespace Modules\admin;
 
+use Template\Template;
+
+class Controller
+{
+    public function index(): void
+    {
+        $tpl = new Template(ROOT_PATH . '/templates/admin');
+        $tpl->render('main.tpl', [
+            'title' => 'Admin',
+            'content' => 'Admin dashboard (stub)',
+        ]);
+    }
+}
