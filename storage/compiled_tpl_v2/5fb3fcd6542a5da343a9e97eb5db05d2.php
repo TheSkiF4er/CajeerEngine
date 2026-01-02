5fda50ac241035f8dcb6fd10447ebd91f66f6bc4
Dmitry Efimov <cajeerskif4er@gmail.com>
2026-01-02 13:25:42 +0400

    v4.0.0-F12

--- storage/compiled_tpl_v2/5fb3fcd6542a5da343a9e97eb5db05d2.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Compiled by CajeerEngine Template DSL
-$__out = '';
-$vars = $vars ?? [];
-
-$__out .= \Template\DSL\Runtime::includeFile('_layout_end.tpl', $vars);
-
-echo $__out;
-return $__out;
