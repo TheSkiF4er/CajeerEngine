f13a80597513177c2c75a57c1425baa15bb1cdcf
SkiF4er <cajeerskif4er@gmail.com>
2026-03-12 15:36:52 +0300

    Add files via upload

--- engine/Core/PublicKernelAPI.php
+++ engine/Core/PublicKernelAPI.php
@@ -1,29 +1,6 @@
 <?php
 namespace Core;
 
-if (!interface_exists(__NAMESPACE__ . '\\KernelContract', false)) {
-    interface KernelContract
-    {
-        public function version(): string;
-        public function registerProvider(ServiceProviderContract $provider): void;
-        public function events(): Events\EventBusContract;
-        public function jobs(): \Jobs\QueueContract;
-        public function get(string $id);
-        public function set(string $id, $service): void;
-    }
-}
-
-if (!interface_exists(__NAMESPACE__ . '\\ServiceProviderContract', false)) {
-    interface ServiceProviderContract
-    {
-        public function register(KernelContract $kernel): void;
-    }
-}
-
-if (!interface_exists(__NAMESPACE__ . '\\PluginContract', false)) {
-    interface PluginContract
-    {
-        public function manifest(): array;
-        public function provider(): ServiceProviderContract;
-    }
-}
+require_once __DIR__ . '/KernelContract.php';
+require_once __DIR__ . '/ServiceProviderContract.php';
+require_once __DIR__ . '/PluginContract.php';
